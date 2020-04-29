fcf6ec87a6858b0b5569e96f6b2592af272cfe32
Lucas Martins <lucas.martins@hexagon.com>
2020-04-29 02:43:16 -0300

    product find by Id

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -19,6 +19,15 @@ router.post('/products', auth, async (req, res) => {
 
 router.get('/products', auth, async (req, res) => {
     try {
+        const products = await Product.find({ _id: req.params.id})
+        res.status(200).send(products)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/product/:id', auth, async (req, res) => {
+    try {
         const products = await Product.find()
         res.status(200).send(products)
     } catch (error) {
